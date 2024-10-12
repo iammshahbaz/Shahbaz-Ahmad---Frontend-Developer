06e49f3e6bc5679326eb6e2b6d945ed83175e8f2
iammshahbaz <shahbaz.ahamed1995@gmail.com>
2024-10-12 19:44:45 +0530

    mainbody

--- matrix/src/App.jsx
+++ matrix/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Body from './components/Body'
+import About from './components/About'
 
 const App = () => {
   return (
     <>
      <Navbar/>
+     <Body/>
+     <About/>
     
     </>
   )
---
+++ matrix/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import body from "../assets/body.png"
+
+const Body = () => {
+  return (
+    <div className="min-h-screen bg-[#00161D] text-white p-8">
+   <img src={body} alt="" />
+  </div>
+  
+  )
+}
+
+export default Body
